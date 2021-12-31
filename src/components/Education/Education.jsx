8ab0a8bb4691cc3625b7f803c98f863a5952e532
soydiego <diegofranchina@gmail.com>
2021-12-31 21:01:30 +0100

    continue with education styles

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -4,7 +4,180 @@ const Education = () => {
 	return (
 		<div className="tw-w-full tw-bg-slate-900">
 			<div className="tw-flex tw-flex-col md:tw-grid tw-grid-cols-9 tw-mx-auto tw-p-8 tw-text-blue-50">
+				{/* <!-- right --> */}
+				<div className="tw-flex md:tw-contents">
+					<div className="tw-col-start-5 tw-col-end-6 tw-mr-10 md:tw-mx-auto tw-relative">
+						<div className="tw-h-full tw-w-6 tw-flex tw-items-center tw-justify-center ">
+							<div className="tw-h-full tw-w-1 tw-bg-pink-600 tw-pointer-events-none"></div>
+						</div>
+						<div className="tw-w-6 tw-h-6 tw-absolute tw-top-1/2 tw--mt-3 tw-rounded-full tw-bg-pink-400 tw-shadow"></div>
+					</div>
+					<div className="tw-bg-slate-600 tw-col-start-6 tw-col-end-10 tw-p-4 tw-rounded-xl tw-my-4 tw-mr-auto tw-shadow-md tw-text-center">
+						<h5 className="tw-leading-tight tw-text-center tw-text-pink-400 tw-font-bold">
+							Abril '20 - Noviembre '21
+						</h5>
+						<h3 className="tw-font-semibold tw-text-lg tw-mb-1">
+							Front-end Developer
+						</h3>
+						<h4 className="tw-font-semibold tw-text-lg tw-mb-1">
+							Turijobs
+						</h4>
+
+						<ul className="tw-list-disc tw-text-left tw-pl-3">
+							<li>Desarrollo Web, actualización de contenidos</li>
+							<li>Optimización, SEO</li>
+							<li>
+								<span className="tw-font-bold">
+									Tecnologías:
+								</span>
+								<p>
+									HTML5, CSS3, Sass, React JS, Redux, GraphQL,
+									Style Components, Storybook, Tailwind,
+									Bootstrap.
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
 				{/* <!-- left --> */}
+				<div className="tw-flex tw-flex-row-reverse md:tw-contents">
+					<div className="tw-bg-slate-600 tw-col-start-1 tw-col-end-5 tw-p-4 tw-rounded-xl tw-my-4 tw-ml-auto tw-shadow-md tw-text-center">
+						<h5 className="tw-leading-tight tw-text-center tw-text-pink-400 tw-font-bold">
+							Septiembre '20 - Abril '21
+						</h5>
+						<h3 className="tw-font-semibold tw-text-lg tw-mb-1">
+							Front-end Developer
+						</h3>
+						<h4 className="tw-font-semibold tw-text-lg tw-mb-1">
+							Labelgroup Networks
+						</h4>
+						<ul className="tw-list-disc tw-text-left tw-pl-3">
+							<li>
+								Desarrollo de ERP para el sector farmaceutico
+							</li>
+							<li>Desarrollo Web</li>
+							<li>
+								<span className="tw-font-bold">
+									Tecnologías:
+								</span>
+								<p>
+									HTML5, CSS3, Sass, React JS, Redux, GraphQL,
+									Style Components, Storybook, Tailwind,
+									Bootstrap.
+								</p>
+							</li>
+						</ul>
+					</div>
+					<div className="tw-col-start-5 tw-col-end-6 md:tw-mx-auto tw-relative tw-mr-10">
+						<div className="tw-h-full tw-w-6 tw-flex tw-items-center tw-justify-center">
+							<div className="tw-h-full tw-w-1 tw-bg-pink-600 tw-pointer-events-none"></div>
+						</div>
+						<div className="tw-w-6 tw-h-6 tw-absolute tw-top-1/2 tw--mt-3 tw-rounded-full tw-bg-pink-400 tw-shadow"></div>
+					</div>
+				</div>
+				{/* <!-- right --> */}
+				<div className="tw-flex md:tw-contents">
+					<div className="tw-col-start-5 tw-col-end-6 tw-mr-10 md:tw-mx-auto tw-relative">
+						<div className="tw-h-full tw-w-6 tw-flex tw-items-center tw-justify-center ">
+							<div className="tw-h-full tw-w-1 tw-bg-pink-600 tw-pointer-events-none"></div>
+						</div>
+						<div className="tw-w-6 tw-h-6 tw-absolute tw-top-1/2 tw--mt-3 tw-rounded-full tw-bg-pink-400 tw-shadow"></div>
+					</div>
+					<div className="tw-bg-slate-600 tw-col-start-6 tw-col-end-10 tw-p-4 tw-rounded-xl tw-my-4 tw-mr-auto tw-shadow-md tw-text-center">
+						<h5 className="tw-leading-tight tw-text-center tw-text-pink-400 tw-font-bold">
+							Julio '19 - Diciembre '19
+						</h5>
+						<h3 className="tw-font-semibold tw-text-lg tw-mb-1">
+							Front-end Developer
+						</h3>
+						<h4 className="tw-font-semibold tw-text-lg tw-mb-1">
+							CDM Barcelona
+						</h4>
+						<ul className="tw-list-disc tw-text-left tw-pl-3">
+							<li>Desarrollo Web, actualización de contenidos</li>
+							<li>Optimización, SEO</li>
+							<li>
+								<span className="tw-font-bold">
+									Tecnologías:
+								</span>
+								<p>
+									HTML5, CSS3, Sass, React JS, Redux, GraphQL,
+									Style Components, Storybook, Tailwind,
+									Bootstrap.
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+				{/* <!-- left --> */}
+				<div className="tw-flex tw-flex-row-reverse md:tw-contents">
+					<div className="tw-bg-slate-600 tw-col-start-1 tw-col-end-5 tw-p-4 tw-rounded-xl tw-my-4 tw-ml-auto tw-shadow-md tw-text-center">
+						<h5 className="tw-leading-tight tw-text-center tw-text-pink-400 tw-font-bold">
+							Mayo '19 - Julio '19
+						</h5>
+						<h3 className="tw-font-semibold tw-text-lg tw-mb-1">
+							Front-end Developer
+						</h3>
+						<h4 className="tw-font-semibold tw-text-lg tw-mb-1">
+							USACD
+						</h4>
+						<ul className="tw-list-disc tw-text-left tw-pl-3">
+							<li>Desarrollo Web, actualización de contenidos</li>
+							<li>Optimización, SEO</li>
+							<li>
+								<span className="tw-font-bold">
+									Tecnologías:
+								</span>
+								<p>
+									HTML5, CSS3, Sass, React JS, Redux, GraphQL,
+									Style Components, Storybook, Tailwind,
+									Bootstrap.
+								</p>
+							</li>
+						</ul>
+					</div>
+					<div className="tw-col-start-5 tw-col-end-6 md:tw-mx-auto tw-relative tw-mr-10">
+						<div className="tw-h-full tw-w-6 tw-flex tw-items-center tw-justify-center">
+							<div className="tw-h-full tw-w-1 tw-bg-pink-600 tw-pointer-events-none"></div>
+						</div>
+						<div className="tw-w-6 tw-h-6 tw-absolute tw-top-1/2 tw--mt-3 tw-rounded-full tw-bg-pink-400 tw-shadow"></div>
+					</div>
+				</div>
+				{/* <!-- right --> */}
+				<div className="tw-flex md:tw-contents">
+					<div className="tw-col-start-5 tw-col-end-6 tw-mr-10 md:tw-mx-auto tw-relative">
+						<div className="tw-h-full tw-w-6 tw-flex tw-items-center tw-justify-center ">
+							<div className="tw-h-full tw-w-1 tw-bg-pink-600 tw-pointer-events-none"></div>
+						</div>
+						<div className="tw-w-6 tw-h-6 tw-absolute tw-top-1/2 tw--mt-3 tw-rounded-full tw-bg-pink-400 tw-shadow"></div>
+					</div>
+					<div className="tw-bg-slate-600 tw-col-start-6 tw-col-end-10 tw-p-4 tw-rounded-xl tw-my-4 tw-mr-auto tw-shadow-md tw-text-center">
+						<h5 className="tw-leading-tight tw-text-center tw-text-pink-400 tw-font-bold">
+							Marzo '16 - Diciembre '16
+						</h5>
+						<h3 className="tw-font-semibold tw-text-lg tw-mb-1">
+							Front-end Developer
+						</h3>
+						<h4 className="tw-font-semibold tw-text-lg tw-mb-1">
+							Everis, Spain
+						</h4>
+						<ul className="tw-list-disc tw-text-left tw-pl-5">
+							<li>Desarrollo Web, actualización de contenidos</li>
+							<li>Optimización, SEO</li>
+							<li>
+								<span className="tw-font-bold">
+									Tecnologías:
+								</span>
+								<p>
+									HTML5, CSS3, Sass, React JS, Redux, GraphQL,
+									Style Components, Storybook, Tailwind,
+									Bootstrap.
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+				{/* <!-- left EDUCATION --> */}
 				<div className="tw-flex tw-flex-row-reverse md:tw-contents">
 					<div className="tw-bg-slate-600 tw-col-start-1 tw-col-end-5 tw-p-4 tw-rounded-xl tw-my-4 tw-ml-auto tw-shadow-md tw-text-center">
 						<h5 className="tw-leading-tight tw-text-center tw-text-pink-400 tw-font-bold">
@@ -16,11 +189,13 @@ const Education = () => {
 						<h4 className="tw-font-semibold tw-text-lg tw-mb-1">
 							Analista de Sistemas
 						</h4>
-						<p className="tw-leading-tight tw-text-center">
-							Legalizado y Certificado en el Ministerio de
-							Relaciones Exteriores para poder trabajar en el
-							Exterior.
-						</p>
+						<ul className="tw-list-disc tw-text-left tw-pl-5">
+							<li>
+								Legalizado y Certificado en el Ministerio de
+								Relaciones Exteriores para poder trabajar en el
+								Exterior.
+							</li>
+						</ul>
 					</div>
 					<div className="tw-col-start-5 tw-col-end-6 md:tw-mx-auto tw-relative tw-mr-10">
 						<div className="tw-h-full tw-w-6 tw-flex tw-items-center tw-justify-center">
@@ -45,9 +220,9 @@ const Education = () => {
 							Nuestra Sra. del Buen y Perpetuo Socorro
 						</h3>
 						<h4>Bachillerato</h4>
-						<p className="tw-leading-tight tw-text-center">
-							Perito Mercantil
-						</p>
+						<ul className="tw-list-disc tw-text-left tw-pl-5">
+							<li>Perito Mercantil</li>
+						</ul>
 					</div>
 				</div>
 			</div>
